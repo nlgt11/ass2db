83c533d5ff479a0cce179b20ecc68117991e87d7
nlgt11 <thuan.ngolegia@gmail.com>
2020-07-10 01:25:19 +0700

    Add flash message

--- routes/courses.js
+++ routes/courses.js
@@ -14,7 +14,24 @@ router.get('/', async (req, res) => {
         let pool = await sql.connect(dbconfig);
         let result = await pool.request()
                             .query(`SELECT * FROM el.Course`);
-        console.log(result);
+
+        res.render('courses/index', { courses: result.recordset, moment: moment });
+
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+router.get('/new', (req, res) => {
+    res.render('courses/new')
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        let pool = await sql.connect(dbconfig);
+        let result = await pool.request()
+                            .query(`SELECT * FROM el.Course`);
+        //console.log(result);
         res.render('courses/index', { courses: result.recordset, moment: moment });
 
     } catch (err) {
@@ -23,42 +40,50 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/', [
-    check('req.body.course.Course_name', 'Name is required').not().isEmpty,
-    check('req.body.course.Price', 'Price is required').not().isEmpty,
-    check('req.body.course.HH', 'Name is required').not().isEmpty,
-    check('req.body.course.MM', 'Name is required').not().isEmpty,
-    check('req.body.course.SS', 'Name is required').not().isEmpty,
+    check('req.body.course.Course_name', 'Tên đang trống').not().isEmpty(),
+    check('req.body.course.Price', 'Giá đang trống').not().isEmpty(),
+    check('req.body.course.HH', 'Thời lượng đang trống').not().isEmpty(),
+    check('req.body.course.MM', 'Thời lượng đang trống').not().isEmpty(),
+    check('req.body.course.SS', 'Thời lượng đang trống').not().isEmpty(),
 ] ,async (req, res) => {
     const result = validationResult(req);
     
-
-    const { Course_name, HH, MM, SS, Price } = req.body.course;
-    const Date_created = Date.now();
-    try {
-        let pool = await sql.connect(dbconfig);
-        // let result = await pool.request()
-        //                         .input('name', sql.NVarChar(100), Course_name)
-        //                         .input('price', sql.Float, Price)
-        //                         .input('length', sql.Time(7), Date(Length))
-        //                         .input('date', sql.Date, Date(Date_created))
-        //                         .query(`INSERT INTO el.Course (Course_name, Length, Price, Rating, Date_created) VALUES (@name, @length, @price, 0, @date);`)
-        let result1 = await pool.request()
-                                .input('name', sql.NVarChar(100), Course_name)
-                                .input('price', sql.Float, Price)
-                                .input('length', sql.Time(7), Date(`${HH}:${MM}:${SS}`))
-                                .input('date', sql.Date, Date(Date_created))
-                                .input('rating', sql.Int, 0)
-                                .execute('el.addDataToCourse')
-        res.redirect('/courses');
-    } catch (e) {
-        console.log(e);
+    if (!result.isEmpty()) {
+        var errors = result.errors;
+        console.log(errors);
+        for (var key in errors) {
+            console.log(errors[key].msg);
+            req.flash("error", errors[key].msg);
+        }
+        return res.redirect('/courses/new')
+    } else {
+        const { Course_name, HH, MM, SS, Price } = req.body.course;
+        const Date_created = Date.now();
+        try {
+            console.log('at leastr i tried')
+            let pool = await sql.connect(dbconfig);
+            // let result = await pool.request()
+            //                         .input('name', sql.NVarChar(100), Course_name)
+            //                         .input('price', sql.Float, Price)
+            //                         .input('length', sql.Time(7), Date(Length))
+            //                         .input('date', sql.Date, Date(Date_created))
+            //                         .query(`INSERT INTO el.Course (Course_name, Length, Price, Rating, Date_created) VALUES (@name, @length, @price, 0, @date);`)
+            let result1 = await pool.request()
+                                    .input('name', sql.NVarChar(100), Course_name)
+                                    .input('price', sql.Float, Price)
+                                    .input('length', sql.Time(7), Date(`${HH}:${MM}:${SS}`))
+                                    .input('date', sql.Date, Date(Date_created))
+                                    .input('rating', sql.Int, 0)
+                                    .execute('el.addDataToCourse')
+            req.flash('success', `Thêm thành công khoá học ${name}`)
+            res.redirect('/courses');
+        } catch (e) {
+            console.log(e);
+        }
     }
 
 })
 
-router.get('/new', (req, res) => {
-    res.render('courses/new')
-});
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const config = require('./config');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const flash = require('connect-flash');
 const methodOverride = require('method-override');
 
 const app = express();
-
+app.set("view engine", "ejs");
 app.use(methodOverride("_method"));
 app.use(bodyParser.urlencoded({extended: true}));
-app.set("view engine", "ejs");
+app.use(cookieParser('keyboard cat'));
+app.use(session({ cookie: { maxAge: 60000 }}));
+app.use(flash());
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
+
 
 //ROUTING
 app.use(require('./routes/index'));
